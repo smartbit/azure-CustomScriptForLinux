50e2e5856eefca82bfb507da364846008a0f052a
Bart Smith <smartbit@users.noreply.github.com>
2017-03-06 16:22:36 +0100

    typical script for ubuntu

--- commandtoexecute.bash
+++ commandtoexecute.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
 # http://stackoverflow.com/questions/35667361/arm-template-deploy-a-customscriptforlinux/35670039#35670039
-echo My First script
-df -h
+echo My First script > /tmp/test.txt
+df -h >> /tmp/test.txt
+echo >> /tmp/test.txt
+mount >> /tmp/test.txt
+apt-get -y update
+apt-get -y upgrade
